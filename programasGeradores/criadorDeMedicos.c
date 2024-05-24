02f2b50a53b111f6003c1e1fb617677dd0f847a3
Arthur <91708847+arthursetragni@users.noreply.github.com>
2024-05-24 16:29:41 -0300

    Add files via upload

--- programasGeradores/criadorDeMedicos.c
+++ programasGeradores/criadorDeMedicos.c
@@ -10,7 +10,7 @@
 typedef struct {
     int crm;
     char nome[50];
-    int idade;
+    char idade[11];
     char cidade[50];
     char especialidade[50];
     int numeroA;
@@ -50,6 +50,25 @@ char *obterNomeAleatorio() {
     return nomes[rand() % NUM_NOMES];
 }
 
+
+// Função para gerar uma data aleatória no formato YYYY-MM-DD
+void gerarDataAleatoria(char *data) {
+    int ano = rand() % (1999 - 1944 + 1) + 1944; // Ano entre 2000 e 2019
+    int mes = rand() % 12 + 1; // Mês entre 1 e 12
+    int dia;
+    if (mes == 2) {
+        // Se for fevereiro, ajustar para até 28 dias
+        dia = rand() % 28 + 1;
+    } else if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
+        // Se for abril, junho, setembro ou novembro, ajustar para até 30 dias
+        dia = rand() % 30 + 1;
+    } else {
+        // Para os outros meses, até 31 dias
+        dia = rand() % 31 + 1;
+    }
+    sprintf(data, "%d-%02d-%02d", ano, mes, dia);
+}
+
 int main() {
     // Abrir o arquivo para escrever
     FILE *arquivo;
@@ -73,13 +92,13 @@ int main() {
         Medico medico;
         medico.crm = 1000 + i; // CRM sequencial a partir de 1000
         strcpy(medico.nome, obterNomeAleatorio());
-        medico.idade = rand() % 46 + 25; // Idade entre 25 e 70
+        gerarDataAleatoria(medico.idade); // Idade entre 25 e 70
         strcpy(medico.cidade, cidades[rand() % 5]); // Escolha aleatória de uma cidade
         strcpy(medico.especialidade, especialidades[rand() % 6]); // Escolha aleatória de uma especialidade
         medico.numeroA = rand() % 1000 + 1; // Número aleatório entre 1 e 1000
 
         // Escrever os dados do médico no arquivo CSV
-        fprintf(arquivo, "%d,%s,%d,%s,%s,%d\n", medico.crm, medico.nome, medico.idade, medico.cidade, medico.especialidade, medico.numeroA);
+        fprintf(arquivo, "%d,%s,%s,%s,%s,%d\n", medico.crm, medico.nome, medico.idade, medico.cidade, medico.especialidade, medico.numeroA);
     }
 
     // Fechar o arquivo
